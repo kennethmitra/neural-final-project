8adb0a81d074939bda57c66595191c81a185f0bb
Kenneth Mitra <kennethmitra@gmail.com>
2021-04-22 22:39:08 -0500

    Add EEG PSD

--- eeg/preprocessing_eeg.py
+++ eeg/preprocessing_eeg.py
@@ -1,6 +1,7 @@
 from scipy.io import loadmat
 from scipy import signal
 from scipy.signal import butter
+from scipy.fft import fft, fftfreq
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,16 +10,19 @@ EPSILON = 1e-12
 
 # 1 second = 512 samples
 
-def _process(run):
+def _process(run, apply_filter=False):
     eeg = run['eeg']
+    if apply_filter:
+        sos = butter(10, (0.1, 20), 'bandpass', fs=512, output='sos')
+        for i in range(eeg.shape[1]):
+            eeg[:, i] = signal.sosfilt(sos, eeg[:, i])
     triggers = run['hdr']['triggers'][0][0]
     indicies = np.array([i for i, x in enumerate(triggers) if x != 0])
     indicies = indicies[3:]
     master_1 = np.zeros(shape=(10, 3584, 32))
     master_2 = np.zeros(shape=(10, 3584, 32))
     zscore = lambda x: (x - np.mean(x)) / (np.std(x) + EPSILON)
-    sos = butter(6, (0.1, 20), 'bandpass', fs=512, output='sos') 
-    eeg = zscore(signal.sosfilt(sos, eeg))
+    eeg = zscore(eeg)
     i = 0
     j = 0
     for x in range(len(indicies)):
@@ -36,8 +40,8 @@ def _process(run):
 
 
 # Extract all MEP's and put them in a matrix for comparison
-def process(subject, session):
-    filename = 'p2_subject{}{}'.format(str(subject), session)
+def process(subject, session, apply_filter=False):
+    filename = '../p2_subject{}{}'.format(str(subject), session)
     data = loadmat(filename + '.mat')
     data = data[filename.split('_')[-1]]
     data = data['MI'][0][0][0]
@@ -46,13 +50,42 @@ def process(subject, session):
     j = 0
     for i in range(3):
         run = data[i]
-        m1, m2 = _process(run)
+        m1, m2 = _process(run, apply_filter)
         for k in range(10):
             motion_1_eeg[j] = m1[k]
             motion_2_eeg[j] = m2[k]
             j += 1
+
+    plot_spectrum(motion_1_eeg, motion_2_eeg, subject, session, apply_filter)
+
     np.save('subject{}{}_motion_1_eeg.npy'.format(str(subject), session), motion_1_eeg)
     np.save('subject{}{}_motion_2_eeg.npy'.format(str(subject), session), motion_2_eeg)
+
+
+def plot_spectrum(motion_1_eeg, motion_2_eeg, subject, session, apply_filter):
+    if session == 'Pre':
+        _session = 1
+    elif session == 'Post':
+        _session = 3
+    
+    for i in range(motion_1_eeg.shape[-1]):
+        _motion_1_eeg = motion_1_eeg[:, :, i]
+        _motion_2_eeg = motion_2_eeg[:, :, i]
+        avg_motion_1_eeg = np.mean(_motion_1_eeg, axis=0)
+        avg_motion_2_eeg = np.mean(_motion_2_eeg, axis=0)
+
+        fig, (ax1, ax2) = plt.subplots(2)
+        fig.suptitle('Subject {} Session {} EEG Channel {}'.format(str(subject), str(_session), str(i)))
+        ax1.psd(avg_motion_1_eeg, Fs=512)
+        ax2.psd(avg_motion_2_eeg, Fs=512)
+        ax1.set_title('Hand Flexion')
+        ax2.set_title('Hand Extension')
+        fig.subplots_adjust(hspace=0.8)
+        fig_name = 'subject_{}_session_{}_channel_{}_eeg_psd'.format(str(subject), str(_session), str(i))
+        if apply_filter:
+            fig_name += '_bandpass_filtered'
+        plt.savefig(fig_name)
+        plt.close()
 
 
 def main():
